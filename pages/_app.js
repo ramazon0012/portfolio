bff06e068274305b031fda7f183a562c29fda155
Ramazon <ergashevrama2010@gmail.com>
2024-03-23 15:36:53 +0500

    fgfu

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,20 @@
-import "@/styles/globals.css";
+import '@/src/css/c482c5e60011444c.css';
+import '../src/chunks/2-4d4fe008758e4991';
+import '../src/chunks/250-df4736cf318585a8';
+import '../src/chunks/749-fd41a9816a63edf4';
+import '../src/chunks/938-97a4c5fe53c20fb7';
+import '../src/chunks/app/layout-3881dfce185fab35';
+import '../src/chunks/app/about/page-4a6ecdc8f5394bd0';
+import '../src/chunks/app/articles/page-2b6ac5b875e3cc20';
+import '../src/chunks/app/page-7edc645259496314.js';
+import '../src/chunks/main-app-ead4ecf24f82c37a.js';
+import '../src/chunks/polyfills-c67a75d1b6f99dc8.js';
+// import '../src/chunks/webpack-e799c267c86ca5b6.js';
+import '../src/chunks/app/speaking/page-435190af63df20bb';
+import '../src/chunks/app/uses/page-604bd1c56a895374';
+import '../src/chunks/app/projects/page-58673c2ba45087d2';
+import '../src/chunks/fd9d1056-e9a01193b1822c33.js';
+import './js.js';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
