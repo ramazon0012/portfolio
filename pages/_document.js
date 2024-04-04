cb26897718a97f38589fee37f6187c300432d198
Ramazon <ergashevrama2010@gmail.com>
2024-04-04 15:10:50 +0500

    tyuhjik

--- pages/_document.js
+++ pages/_document.js
@@ -7,17 +7,17 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="stylesheet" href="/css/c482c5e60011444c.css" />
-          <script src='/chunks/2-4d4fe008758e4991.js' />
-          <script src="/chunks/2-4d4fe008758e4991.js" />
-          <script src="/chunks/250-df4736cf318585a8.js" />
-          <script src="/chunks/749-fd41a9816a63edf4.js" />
-          <script src="/chunks/main-app-ead4ecf24f82c37a.js" />
-          <script src="/chunks/polyfills-c67a75d1b6f99dc8.js" />
-          <script src="/chunks/app/speaking/page-435190af63df20bb.js" />
-          <script src="/chunks/app/uses/page-604bd1c56a895374.js" />
-          <script src="/chunks/app/projects/page-58673c2ba45087d2.js" />
-          <script src="/chunks/fd9d1056-e9a01193b1822c33.js" />
-          <script src="/js.js" />
+          <script src='/chunks/2-4d4fe008758e4991.js'></script>
+          <script src="/chunks/2-4d4fe008758e4991.js"></script>
+          <script src="/chunks/250-df4736cf318585a8.js"></script>
+          <script src="/chunks/749-fd41a9816a63edf4.js"></script>
+          <script src="/chunks/main-app-ead4ecf24f82c37a.js"></script>
+          <script src="/chunks/polyfills-c67a75d1b6f99dc8.js"></script>
+          <script src="/chunks/app/speaking/page-435190af63df20bb.js"></script>
+          <script src="/chunks/app/uses/page-604bd1c56a895374.js"></script>
+          <script src="/chunks/app/projects/page-58673c2ba45087d2.js"></script>
+          <script src="/chunks/fd9d1056-e9a01193b1822c33.js"></script>
+          <script src="/js.js"></script>
         </Head>
         <body>
           <Main />
